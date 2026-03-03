4fec16f342b106702e8ae611b53cf7712c45d486
dev2 <dev2@test.com>
2026-03-03 15:26:22 +0800

    fix: use cdn br wasm paths for production and preload

--- config/prod.env.ts
+++ config/prod.env.ts
@@ -2,8 +2,8 @@ const BASE_DOMAIN = '47.96.65.146'
 const CHAT_URL = `http://${BASE_DOMAIN}/chat`
 const API_URL = `http://${BASE_DOMAIN}/api`
 const WS_URL = `ws://${BASE_DOMAIN}/msg_gateway`
-const WASM_URL = `http://${BASE_DOMAIN}/openIM.wasm`
-const SQL_WASM_URL = `http://${BASE_DOMAIN}/sql-wasm.wasm`
+const WASM_URL = 'https://www.gongyangame.com/openim/openIM.wasm.br'
+const SQL_WASM_URL = 'https://www.gongyangame.com/openim/sql-wasm.wasm.br'
 
 export default {
   NODE_ENV: 'production',
